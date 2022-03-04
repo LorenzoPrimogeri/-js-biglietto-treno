2710261f6bd6bf1886607eaa6240a41e00740c04
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-04 14:56:01 +0100

    Complete with a little chage

--- javascript.js
+++ javascript.js
@@ -5,23 +5,26 @@ const PrezzoBiglietto = 0.21;
 let Sconto20 = (PrezzoBiglietto * NumeroKm * 20 / 100);
 let Sconto40 = (PrezzoBiglietto * NumeroKm * 40 / 100);
 let PrezzoFinale;
-// const PrezzoBigliettoMinorenni = 
-// const PrezzoBigliettoOver65 = 
-// console.log(PrezzoBigliettoMinorenni + PrezzoBigliettoOver65)
+
 if (NumeroKm != isNaN || Eta != isNaN) {
     if (Eta < 18) {
         PrezzoFinale = (PrezzoBiglietto * NumeroKm - Sconto20);
         PrezzoFinale = (Math.round(PrezzoFinale * 100) / 100).toFixed(2);
+        document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale (con lo sconto del 20%) è: " + PrezzoFinale + " $";
+
     } if (Eta > 18 && Eta < 65) {
         PrezzoFinale = (PrezzoBiglietto * NumeroKm);
         PrezzoFinale = (Math.round(PrezzoFinale * 100) / 100).toFixed(2);
+        document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale è: " + PrezzoFinale + " $";
+
     }
     if (Eta > 65) {
         PrezzoFinale = (PrezzoBiglietto * NumeroKm - Sconto40);
         PrezzoFinale = (Math.round(PrezzoFinale * 100) / 100).toFixed(2);
-    } else {
-        alert("Lo sai che ti ho chiesto dei numeri?");
+        document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale (con lo sconto del 40%) è: " + PrezzoFinale + " $";
+
     }
+} else {
+    alert("Lo sai che ti ho chiesto dei numeri?");
 }
 
-document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale è: " + PrezzoFinale + " $";
