919a06c00098c3690b86d57f70cd9e1c3b6d0c9f
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-04 15:12:28 +0100

    Adding control on parametr

--- javascript.js
+++ javascript.js
@@ -6,25 +6,22 @@ let Sconto20 = (PrezzoBiglietto * NumeroKm * 20 / 100);
 let Sconto40 = (PrezzoBiglietto * NumeroKm * 40 / 100);
 let PrezzoFinale;
 
-if (NumeroKm != isNaN || Eta != isNaN) {
-    if (Eta < 18) {
-        PrezzoFinale = (PrezzoBiglietto * NumeroKm - Sconto20);
-        PrezzoFinale = (Math.round(PrezzoFinale * 100) / 100).toFixed(2);
-        document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale (con lo sconto del 20%) è: " + PrezzoFinale + " $";
 
-    } if (Eta > 18 && Eta < 65) {
-        PrezzoFinale = (PrezzoBiglietto * NumeroKm);
-        PrezzoFinale = (Math.round(PrezzoFinale * 100) / 100).toFixed(2);
-        document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale è: " + PrezzoFinale + " $";
-
-    }
-    if (Eta > 65) {
-        PrezzoFinale = (PrezzoBiglietto * NumeroKm - Sconto40);
-        PrezzoFinale = (Math.round(PrezzoFinale * 100) / 100).toFixed(2);
-        document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale (con lo sconto del 40%) è: " + PrezzoFinale + " $";
-
-    }
-} else {
+if (NumeroKm == isNaN || Eta == isNaN) {
     alert("Lo sai che ti ho chiesto dei numeri?");
+} else if (Eta < 18) {
+    PrezzoFinale = (PrezzoBiglietto * NumeroKm - Sconto20);
+    PrezzoFinale = (Math.round(PrezzoFinale * 100) / 100).toFixed(2);
+    document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale (con lo sconto del 20%) è: " + PrezzoFinale + " $";
+} else if (Eta > 18 && Eta < 65) {
+    PrezzoFinale = (PrezzoBiglietto * NumeroKm);
+    PrezzoFinale = (Math.round(PrezzoFinale * 100) / 100).toFixed(2);
+    document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale è: " + PrezzoFinale + " $";
+} else if (Eta > 65) {
+    PrezzoFinale = (PrezzoBiglietto * NumeroKm - Sconto40);
+    PrezzoFinale = (Math.round(PrezzoFinale * 100) / 100).toFixed(2);
+    document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale (con lo sconto del 40%) è: " + PrezzoFinale + " $";
 }
+
+
 
