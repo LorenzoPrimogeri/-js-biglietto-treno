a0ad898208899387f45f94df9f5c495a32d4a650
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-08 18:37:22 +0100

    layout change

--- javascript.js
+++ javascript.js
@@ -1,26 +1,27 @@
-
-const numerokm = prompt("quanti km vuole percorere?", 10);
-const eta = prompt("qual'è la sua eta?");
-const prezzobiglietto = 0.21;
-let sconto20 = (prezzobiglietto * numerokm * 20 / 100);
-let sconto40 = (prezzobiglietto * numerokm * 40 / 100);
-let prezzofinale;
-
-if (isNaN(numerokm) || isNaN(eta)) {
-    alert("Lo sai che ti ho chiesto dei numeri?");
-} else if (eta < 18) {
-    prezzofinale = (prezzobiglietto * numerokm - sconto20);
-    prezzofinale = (Math.round(prezzofinale * 100) / 100).toFixed(2);
-    document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale (con lo sconto del 20%) è: " + prezzofinale + " $";
-} else if (eta > 18 && eta < 65) {
-    prezzofinale = (prezzobiglietto * numerokm);
-    prezzofinale = (Math.round(prezzofinale * 100) / 100).toFixed(2);
-    document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale è: " + prezzofinale + " $";
-} else if (eta > 65) {
-    prezzofinale = (prezzobiglietto * numerokm - sconto40);
-    prezzofinale = (Math.round(prezzofinale * 100) / 100).toFixed(2);
-    document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale (con lo sconto del 40%) è: " + prezzofinale + " $";
+const button = document.getElementById("btn");
+button.addEventListener('click', function () {
+    const numerokm = document.getElementById("numerokm").value;
+    const eta = document.getElementById("eta").value;
+    const prezzobiglietto = 0.21;
+    const sconto20 = (prezzobiglietto * numerokm * 20 / 100);
+    const sconto40 = (prezzobiglietto * numerokm * 40 / 100);
+    let prezzofinale;
+    if (isNaN(numerokm) || isNaN(eta)) {
+        alert("Lo sai che ti ho chiesto dei numeri?");
+    } else if (eta < 18) {
+        prezzofinale = (prezzobiglietto * numerokm - sconto20);
+        prezzofinale = (Math.round(prezzofinale * 100) / 100).toFixed(2);
+        document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale (con lo sconto del 20%) è: " + prezzofinale + " $";
+    } else if (eta > 18 && eta < 65) {
+        prezzofinale = (prezzobiglietto * numerokm);
+        prezzofinale = (Math.round(prezzofinale * 100) / 100).toFixed(2);
+        document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale è: " + prezzofinale + " $";
+    } else if (eta > 65) {
+        prezzofinale = (prezzobiglietto * numerokm - sconto40);
+        prezzofinale = (Math.round(prezzofinale * 100) / 100).toFixed(2);
+        document.getElementById("PrezzoFinale").innerHTML = "Il prezzo finale (con lo sconto del 40%) è: " + prezzofinale + " $";
+    }
 }
-
+);
 
 
